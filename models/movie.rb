9d9fd4ba0150a729a4f8b283b5ea65f21e468d54
Ross Lorimer <wross.lorimer@googlemail.com>
2019-09-26 15:39:45 +0100

    Update methods added

--- models/movie.rb
+++ models/movie.rb
@@ -22,10 +22,21 @@ class Movie
     @id = id_hash['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE movies SET (title, genre)
+    = ($1, $2)
+    WHERE
+    id = $3"
+    values = [@title, @genre, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * FROM movies"
     movies = SqlRunner.run(sql)
     return movies.map{|movie| Movie.new(movie)}
   end
+
+
 
 end

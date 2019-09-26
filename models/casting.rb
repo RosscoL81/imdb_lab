af36e7c580ff644392e49ff0565f58f4e56ffa2b
Ross Lorimer <wross.lorimer@googlemail.com>
2019-09-26 14:58:16 +0100

    Classes set-up

--- models/casting.rb
+++ models/casting.rb
@@ -1,3 +1,16 @@
+require_relative("../db/sql_runner")
+
 class Casting
+
+  attr_accessor :movie_id, :star_id, :fee
+  attr_reader :id
+
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @movie_id = options['movie_id']
+    @star_id = options['star_id']
+    @fee = options['fee'].to_i
+  end
 
 end
--- models/movie.rb
+++ models/movie.rb
@@ -1,3 +1,14 @@
+require_relative("../db/sql_runner")
+
 class Movie
+
+  attr_accessor :title, :genre
+  attr_reader :id 
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @genre = options['genre']
+  end
 
 end
